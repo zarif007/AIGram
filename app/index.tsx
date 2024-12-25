cf5f29e02e3f6f8a552bf2023c181e7e8a8b5458
zarif007 <zarifhuq786@gmail.com>
2024-12-25 12:37:53 +0600

    feat: add native wind

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const App = () => {
+  return (
+    <View className="bg-white dark:bg-black flex justify-center items-center">
+      <Text className="text-4xl text-black dark:text-white">App</Text>
+    </View>
+  );
+};
+
+export default App;
